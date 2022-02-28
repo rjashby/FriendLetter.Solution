21cba12e4513cf87529204955783bb498992639e
Ryan Ashby <rjapsu160@gmail.com>
2022-02-27 18:50:43 -0800

    add view and letter.cshtml files along with lesson

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -5,9 +5,14 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
+
+    s[Route("/")]
+    public ActionResult Letter() { return View(); }
 
   }
 }
